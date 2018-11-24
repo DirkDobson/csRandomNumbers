fc34688ed63a35f5b6acacd1f3921062bdb04446
Dirk Dobson <dirkvancouver@live.com>
2018-11-23 19:17:22 -0700

    length new variable

--- Program.cs
+++ Program.cs
@@ -8,8 +8,10 @@ namespace CS
         {
             var random = new Random();
 
-            var buffer = new char[10];
-            for (var i = 0; i < 10; i++)
+            const int passwordLength = 10;
+            
+            var buffer = new char[passwordLength];
+            for (var i = 0; i < passwordLength; i++)
                 buffer[i] = (char)('a' + random.Next(0, 26));
             var password = new string(buffer);
             Console.WriteLine(password);
